e09d96f76884dbcea95e54cd6d5dad2d63080919
Vicente Viera <plantedcoot706@gmail.com>
2022-08-25 09:05:17 -0500

    Folders created and a failed attemp to try to set up tailwind

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <TailwindProvider>
+            <View className="flex-1 items-center justify-center bg-white">
+                <Text className="text-red-900">Open up App.js to start working on your app!</Text>
+                <StatusBar style="auto" />
+            </View>
+        </TailwindProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.js", "./src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: ["tailwindcss-react-native/babel"]
+}
