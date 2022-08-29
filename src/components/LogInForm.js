e8c58bf23218cf5f26eca81108310e1f57d33291
Vicente Viera <plantedcoot706@gmail.com>
2022-08-28 21:56:23 -0500

    merge with login

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import { Input } from "@rneui/themed";
+import { Formik } from 'formik';
 
 const LogInForm = () => {
+    const handleSubmit = async(values) =>{
+        console.log(values)
+    }
+
   return (
-    <>
-        <View>
-            <Input/>
-        </View>
-    </>
+    <View>
+        <Formik
+            initialValues={{
+                email:"",
+                password:""
+            }
+        }
+        onSubmit={async (values, {resetForm}) => {
+            await handleSubmit(values);
+            resetForm();
+        }}
+        >
+            <View  style={{with:"100%",height:40}}>
+                <Input
+                    placeholder="Correo"
+                    style={{with:50,height:20}}
+                />
+                <Input
+                    placeholder="Contraseña"
+                    style={{with:50,height:20}}
+                />
+            </View>
+        </Formik>    
+    </View>
   )
 }
 
