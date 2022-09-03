23cfc66b5e473a9fce5921599c1fbba3cf49e9cf
Luis <79459748+luisalcab@users.noreply.github.com>
2022-09-03 17:49:03 -0500

    Product List Advances

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/layouts/Login';
-import Stripe from './src/layouts/Stripe';
 
 //Component incharge of crating the screens
 const Stack = createNativeStackNavigator(); 
@@ -12,7 +11,7 @@ export default function App() {
     return (
         <NavigationContainer initialRouteName="Login">
             <Stack.Navigator>
-                <Stack.Screen name="Login" component={Stripe} />
+                <Stack.Screen name="Login" component={Login} />
             </Stack.Navigator>
         </NavigationContainer>
     );
---
+++ src/layouts/ListProduct.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const Lista = () => {
+    
