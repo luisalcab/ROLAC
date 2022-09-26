d0fd0ef822ecd1eee4b112ec5f5a49797821abb2
Glitch-loop <renetdejesus@gmail.com>
2022-09-26 12:57:18 -0500

    Add stripe functionality

--- src/layouts/Cart.js
+++ src/layouts/Cart.js
@@ -8,7 +8,7 @@ import PaymentScreen from "../components/stripe/PaymentScreen";
 
 const Cart = ({navigation}) => {
     const {cart} = useContext(CartContext);
-    
+    console.log(publishableKey);
     const nav2QR = () => {
         navigation.navigate("QRGenerator");
     }
@@ -51,7 +51,7 @@ const Cart = ({navigation}) => {
                 </View>
             </View>
             <StripeProvider
-                publishableKey={publishableKey}>
+                publishableKey="pk_test_51LkUu8L3fb2NBnm32ovLcCuet2FDgfprjfA1lAaL0cqZ8SdJHzS1v7erGYck9PWWpY43cfquaZAJUudpNihX0bqu00WVCmQvro">
                 <PaymentScreen grandTotal = { (Math.round(grandTotal() * 100)/ 100).toFixed(2) }/>
             </StripeProvider>
             {/* <View style={styles.footer}>
